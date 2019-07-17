65e420a2b99f7fbdf4740baea47a7fa58213ed5b
Lukas Pioch <lukas@zgow.de>
2019-07-17 19:33:23 +0200

    Issue #3994 has been fixed

--- tables.lua
+++ tables.lua
@@ -242,7 +242,4 @@ function CreateSharedIgnoreTable()
 	-- This functions are missing return types in APIDoc
 	g_IgnoreShared.cRoot.ForEachWorld = true
 	g_IgnoreShared.cRoot.ForEachPlayer = true
-
-	-- Crashes the server: #3994
-	g_IgnoreShared.cEntity.Destroy = true
 end
